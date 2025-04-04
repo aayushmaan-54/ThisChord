fe2fd1f8d92eaaa46b70c46a1759c14cda2d9b9a
Aayushmaan Soni <aayushmaan.soni54@gmail.com>
2025-04-04 20:59:19 +0530

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+// import app from './app';
+// import config from './config/config';
+
+// app.listen(config.port, () => {
+//   console.log(`Server running on port ${config.port}`);
+// });
+
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log("RES: ", add(3, 5));
